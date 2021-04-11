30c9b4ffe51707076faaae0735888c69908d39eb
Maira Galvao <mairagalvao01@gmail.com>
2021-04-11 13:37:12 +0300

    work finished

---
+++ W2/Day1/exercises_xp.js
@@ -0,0 +1,29 @@
+let calc_5_34 = 5 + "34" 
+// = "534"
+let calc_5_4 = 5 - "4" 
+// = 1
+let calc_10_5 = 10 % 5 
+// = 0 (the reminder of 10/5 is 0)
+let calc_5_10 = 5 % 10 
+// = 5 (the reminder of 5/10 is 5) 
+let calc_java_script = "Java" + "Script" 
+// = JavaScript (the result is literatally the words together as a string)
+let calc_quotes = " " + " " 
+// = "  " (the first value is just a space and the second value is also a space, so the result would be two spaces)
+let calc_quotes_0 = " " + 0 
+// = " 0" (the first value is just a space, so the result would be space then the number 0)
+let calc_true_true = true + true 
+// = 2 (false = 0 and true = 1, so 1+1 = 2)
+let calc_true_false = true + false 
+// = 1 (false = 0 and true = 1, so 1+0 = 1)
+let calc_false_true = false + true 
+// = 1 (false = 0 and true = 1, so 0+1 = 1)
+let calc_false_minus_true = false - true 
+// = -1 (false = 0 and true = 1, so 0-1 = -1)
+let calc_3_minus_4 = 3 - 4 
+// = -1 (normal math calculation)
+let bob_plus_bill = "Bob" - "bill" 
+// = nan (no way to do a calculation with minus when the values are strings)
+
+
+
