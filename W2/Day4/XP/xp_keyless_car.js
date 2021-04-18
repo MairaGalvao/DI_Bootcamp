de507523d5de6b67946ec9824c438b4705a2b385
Maira Galvao <mairagalvao01@gmail.com>
2021-04-18 09:30:38 +0300

    work finished

---
+++ W2/Day4/XP/xp_keyless_car.js
@@ -0,0 +1,51 @@
+//Exercise 1 Keyless Car
+
+// let usersName = prompt("What is your name?");
+// let usersAge = parseInt(prompt("What is your age?"));
+
+// function checkDriverAge(age){
+//     if (usersAge < 18){
+//         alert(usersName + " Sorry, you are too young to drive this car. Powering off")
+//     }
+//     else if (usersAge > 18){
+//         alert(usersName + " You are old enough to drive, Powering On. Enjoy the ride!")
+//     }
+//     else if (usersAge == 18){
+//         alert(usersName + " Congratulations on your first year of driving. Enjoy the ride!")
+//     }
+// }   
+// checkDriverAge(20)
+
+// Exercise 2 What’s In My Wallet ?
+
+// let quarters  = 0.25
+// let dimes = 0.10
+// let nickels = 0.05
+// let pennies = 0.01
+// let usersQuarters = parseInt(prompt("How many quarters do you have?"));
+// let usersDimes = parseInt(prompt("How many dimes do you have?"));
+// let usersNickels = parseInt(prompt("How many nickels do you have?"));
+// let usersPennies = parseInt(prompt("How many pennies do you have?"));
+// let costProduct = parseInt(prompt("How much this product cost?"));
+
+// let totalAmoutUser = (usersQuarters * quarters) + (usersDimes * dimes)
+//      + (usersNickels * nickels) + (usersPennies * pennies);
+// let difference = costProduct - totalAmoutUser    
+
+
+// function changeEnough(){
+//     if (totalAmoutUser >= costProduct){
+//         console.log("Amazing, you can buy this product! You have left" + difference + 
+//         "in your acount");}
+//     else if (totalAmoutUser < costProduct){
+//         console.log("Ohh!! Unfortunatly you can not buy this product!You still need" + difference)
+//     }    
+//     }           
+
+// changeEnough()
+
+// Exercise 4 Multiple of 23
+
+function multipleOf23(){
+    
+}
