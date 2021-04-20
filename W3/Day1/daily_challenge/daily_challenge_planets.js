ca17c69404503f69bc048251b98e6b184160e258
Maira Galvao <mairagalvao01@gmail.com>
2021-04-20 10:10:16 +0300

    work finished

--- W3/Day1/daily_challenge/daily_challenge_planets.js
+++ W3/Day1/daily_challenge/daily_challenge_planets.js
@@ -5,7 +5,7 @@ let planetsSolarSystem = ["Mercury", "Venus", "Earth", "Mars",
 
 let gettingSection = document.getElementsByClassName("listPlanets");
 
-    for (let i = 0; i < planetsSolarSystem.length; i++){
+    for (let i = 0; i <= planetsSolarSystem.length; i++){
         let createDiv = document.createElement("div");
         let divText = document.createTextNode(planetsSolarSystem[i]);
         createDiv.classList.add("planet")
@@ -14,46 +14,4 @@ let gettingSection = document.getElementsByClassName("listPlanets");
         gettingSection[0].appendChild(createDiv);
     }
 }
-
-
-// function startingMoonSystem(){
-//     let moon = [{"name": "Mercury", "moons": 1}, 
-//     {"name": "Venus", "moons": 2},
-//      {"name": "Earth", "moons": 3},
-//      {"name": "Venus", "moons": 4},
-//      {"name": "Earth", "moons": 5},
-//      {"name": "Venus", "moons": 6},
-//      {"name": "Earth", "moons": 7},
-//      {"name": "Venus", "moons": 8},
-//      {"name": "Earth", "moons": 9}] 
-//     let gettingSectionforMoon = document.getElementsByClassName("listPlanets");
-  
-    
-//     for (let i = 0; i < moon.length; i++){
-//         let starObj = moon[i];
-//         let starName = starObj.name;
-//         let numberOfMoons = starObj.moons;
-//         let divClassName = "planet " + starName;
-        
-//         // wrap each star with another div (so the moons will be together with it)
-//         let starDiv = document.getElementsByClassName(divClassName)
-//         let starDivWrap = document.createElement("div");
-//         starDivWrap.appendChild(starDiv[0])
-        
-//         console.log(gettingSectionforMoon[0])
-
-//         // creat moons!
-//         let createMoon = document.createElement("div");
-//         let moonText = document.createTextNode(numberOfMoons);
-//         // createMoon.classList.add("planet")
-//         // createMoon.classList.add(moon[i])
-//         createMoon.appendChild(moonText);
-//         console.log(createMoon)
-//         gettingSectionforMoon[0].appendChild(createMoon);
-//     }
-//     // let gettingSection = document.getElementsByClassName("listPlanets");
-//     console.log(gettingSection[0])
-// }
-
 startingSolarSystem()
-// startingMoonSystem()
---
+++ W3/Day2/Daily_challenge_tell_the_story/daily_challenge_tell_story.js
@@ -0,0 +1,8 @@
+const newWords = document.querySelector('[data-form-button]')
+const input = document.querySelector('[data-form-input]')
+
+newWords.addEventListener('click',()=>{
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    console.log(valor)
+})
