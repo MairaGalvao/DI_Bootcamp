44e0603c59189da82be350b324c82bf58d920014
Maira Galvao <mairagalvao01@gmail.com>
2021-04-20 19:30:33 +0300

    work finished

--- W3/Day2/Daily_challenge_tell_the_story/daily_challenge_tell_story.js
+++ W3/Day2/Daily_challenge_tell_the_story/daily_challenge_tell_story.js
@@ -1,8 +1,55 @@
-const newWords = document.querySelector('[data-form-button]')
-const input = document.querySelector('[data-form-input]')
+let getElementButton = document.getElementById("lib-button")
+getElementButton.addEventListener("click", getValueInputs)
 
-newWords.addEventListener('click',()=>{
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
-    console.log(valor)
-})
+let randomButton = document.getElementById("lib-random")
+randomButton.addEventListener("click", generateRandomStory)
+
+let getElementLi = document.getElementsByTagName('li') 
+
+function getInputs() {
+    let storyArray = []
+    for (let index = 0; index < getElementLi.length; index ++){
+        let item = getElementLi[index]          // <li><input>a</input></li>
+        let currentInputValue = item.childNodes[1].value
+        if (currentInputValue == null || currentInputValue == '' ) {
+            alert("Please, make sure you fill all the blanks")
+            break;
+        }
+        storyArray.push(currentInputValue) 
+    }
+    return storyArray
+}
+
+function generateRandomStory() {
+
+    // get the 5 inputs
+    let storyArray = getInputs()
+
+    // create a few templates
+    // put the words in the template (0 = noun, 1 = adj, 2 = name, 3 = verb, 4 = place)
+    let story1 = `It's the worst thing when a ${storyArray[1]} ${storyArray[0]} is ${storyArray[3]} in the middle of ${storyArray[4]} in front of ${storyArray[2]}`
+    let story2 = `When ${storyArray[2]} is ${storyArray[3]} in ${storyArray[4]}, it is so ${storyArray[1]} that the ${storyArray[0]} explodes`
+    let story3 = `No one can resist a nice ${storyArray[1]} ${storyArray[0]} specially when you are ${storyArray[3]} in ${storyArray[4]}, right ${storyArray[2]}?`
+    let storyCatalog = [story1, story2, story3]
+
+    // pick (random) one template
+    const randomIndex = Math.floor(Math.random() * storyCatalog.length);
+    let chosenStory = storyCatalog[randomIndex]
+
+    // push inside of ...
+    let getElementP = document.getElementById('story')
+    getElementP.childNodes[0].nodeValue = chosenStory
+}
+
+function getValueInputs() {
+    let storyArray = getInputs()
+    
+    // insert words in story template (0 = noun, 1 = adj, 2 = name, 3 = verb, 4 = place)
+    let story = `${storyArray[2]}'s ${storyArray[1]} ${storyArray[0]} likes ${storyArray[3]} in ${storyArray[4]}`
+    
+    // insert story into the div
+    let getElementP = document.getElementById('story')
+    newContent = document.createTextNode(story)
+    getElementP.appendChild(newContent)
+}
+
