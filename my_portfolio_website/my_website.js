294985392908031224a6ae59496a7ba60050d886
Maira Galvao <mairagalvao01@gmail.com>
2021-05-14 12:41:34 +0300

    work done

---
+++ my_portfolio_website/my_website.js
@@ -0,0 +1,27 @@
+// //a nav bar with Home, About me, PT, Hebrew, English
+// //a background pic 
+// // div with stuff written on the side
+// //scrow down and then another picture of coding 
+// //a list or another nav bar with options for my skills and my projects (4 projects)
+// //a div with a box and a msg (Emicida)
+// //icons for git, medium, and linkedin
+// //below icons for about me, contact me, et
+// //made with love* a msg on footer
+
+// function creatingNavBar(){
+//     let divNavBar = document.createElement('DIV')
+//     divNavBar.id = ('topnav')
+//     document.body.appendChild(divNavBar)
+// }
+// creatingNavBar()
+
+// function creatingInfoNavBar(){
+//     var navInfo = ["Home", "About", "Contact Me"];
+//     navInfo.forEach(function(text) {
+//     var a = document.createElement("a");
+//     a.className = "navInfo";
+//     a.innerHTML = text;
+//     let myDiv = document.getElementById('topnav')
+//     myDiv.appendChild(a);
+// })}
+// creatingInfoNavBar()
